4eb623daf4a44432605ea26e9fea38c03ab61793
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-03-06 00:53:36 +1000

    Refactor password check program to use functions

--- practical/week3/password_check.py
+++ practical/week3/password_check.py
@@ -1,19 +1,33 @@
 min_length = 10
-password = input("password : ")
-password_length = False
-
-while not password_length:
-
-    if len(password) >= 10:
-        length = len(password)
-        print("*"*length)
-        password_length = True
-
-    elif len(password) < 10:
-        password = input("password : ")
-        password_length = False
 
 
+def main():
+
+    password = get_password()
+    password_length = False
+
+
+    while not password_length:
+
+        if len(password) >= 10:
+            print_password = "*" * len(password)
+            the_password(print_password)
+            password_length = True
+
+        elif len(password) < 10:
+            password = get_password()
+            password_length = False
+
+
+def get_password():
+    password = input("password : ")
+    return password
+
+def the_password(print_password):
+    print(print_password)
+
+
+main()
 
 
 
