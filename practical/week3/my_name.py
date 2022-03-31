5c1444392bd5cb5e72a8a6a3500aaeb5f7c6b35f
Jihyung Park <100411773+Jihyung-PArk@users.noreply.github.com>
2022-03-31 23:17:48 +1000

    Delete practical directory

--- practical/week3/my_name.py
+++
@@ -1,3 +0,0 @@
-"""
-Jihyung Park
-"""
--- practical/week6/guitar_test.py
+++
@@ -1,7 +0,0 @@
-from guitar import Guitar
-
-guiter1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
-
-print(guiter1)
-print(guiter1.get_age())
-print(guiter1.is_vintage())
