c3c4c091b5b6d7c3c4e72a40871c482294fde40b
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-04-05 14:37:48 +1000

    version 1.0.0

---
+++ lecture/week7/do_this_now.py
@@ -0,0 +1,17 @@
+def list_of_name(name_to_age, given_age):
+    # names = []
+    # for key in name_to_age.keys():
+    #     if name_to_age[key] <= given_age:
+    #         names.append(key)
+
+    # return names
+
+    return [key for key in name_to_age.keys() if name_to_age[key] <= given_age]
+
+def main():
+    name_to_age = {"Bill": 21, "Jane": 34, "Sven": 56}
+    print(list_of_name(name_to_age, 30))
+
+
+if __name__ == '__main__':
+    main()
