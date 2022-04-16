61daa12cfa30fae492411e9037738a1065e6bc3f
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-04-17 00:26:59 +1000

    version1.0.0

--- practical/week8/eco_taxi.py
+++ practical/week8/eco_taxi.py
@@ -1,5 +1,19 @@
 from taxi import Taxi
 
-class EcoTaxi:
+class EcoTaxi(Taxi):
 
-    def
+    def __init__(self, name, fuel):
+        super().__init__(name, fuel)
+
+    def __str__(self):
+        return "{}".format(super().__str__())
+
+    def discount_price(self):
+        price_per_km = super().price_per_km
+        return price_per_km / 0.8
+
+    def half_fuel(self):
+        fuel = super().fuel
+        return fuel / 2
+
+
---
+++ practical/week8/eco_taxi_test.py
@@ -0,0 +1,7 @@
+from eco_taxi import EcoTaxi
+
+
+new_taxi = EcoTaxi("Prius 1", 100)
+print(new_taxi)
+
+print(new_taxi.discount_price())
