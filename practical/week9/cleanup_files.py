93d93fb772cf5c9d0a55677379ff96b520079d6a
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-04-23 20:42:21 +1000

    version1.0.0

--- practical/week9/cleanup_files.py
+++ practical/week9/cleanup_files.py
@@ -12,38 +12,37 @@ def main():
         for filename in filenames:
             new_name = get_fixed_filename(filename)
             print("Renaming {} to {}".format(filename, new_name))
-            new_name_two = get_fixed_filename_two(filename)
-            print("Renaming {} to {}".format(filename, new_name_two))
-            # current_name = os.path.join(directory_name, filename)
-            # new_name = os.path.join(directory_name, new_name)
-            # os.rename(current_name, new_name)
+
+            new_name_two = get_fixed_filename_two(new_name)
+            print("Renaming {} to {}".format(new_name, new_name_two))
+            current_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(current_name, new_name)
 
 
-def get_fixed_filename_two(filename):
-    character_num = 1
+def get_fixed_filename_two(new_name):
+
     character = []
-    for characters in enumerate(filename):
+    for characters in enumerate(new_name):
         character.append(characters)
 
-    while character_num == len(character):
-        for i in range(len(character) - 1):
+    for i in range(len(character) - 1):
 
-            if character[i][1].islower() and character[i + 1][1].isupper():
-                new_name = filename.replace(character[i + 1][1], "_" + character[i + 1][1])
+        if character[i][1].islower() and character[i + 1][1].isupper():
+            new_name = new_name.replace(character[i + 1][1], "_" + character[i + 1][1])
+            return new_name
 
-            elif character[i][1].isupper() and character[i + 1][1].isupper():
-                new_name = filename.replace(character[i + 1][1], "_" + character[i + 1][1])
-
-            character_num += 1
-
-
+        if character[i][1].isupper() and character[i + 1][1].isupper():
+            new_name = new_name.replace(character[i + 1][1], "_" + character[i + 1][1])
+            return new_name
+        else:
+            continue
 
 
 def get_fixed_filename(filename):
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
     return new_name
 
-    return new_name
 
 
 main()
